1eccfa00479e69e7816294aafc2ca588b93acc1d
Sereyvuth <sereyvuthc@gmail.com>
2020-02-07 11:52:15 +0100

    routes added

--- initialization/__init__.py
+++ initialization/__init__.py
@@ -1,7 +1,9 @@
+import os
 import sqlalchemy as db
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = db.create_engine('sqlite:///galaxies.db?check_same_thread=False')
+###engine = db.create_engine('sqlite:///galaxies.sqlite') 
 connection = engine.connect()
 metadata = db.MetaData()
 messier = db.Table('messier', metadata, autoload=True, autoload_with=engine)
@@ -9,12 +11,53 @@ db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False,bind=
 
 class Messier:
     def __init__(self):
-        # query should be called before any other method to create proxy connection
-        metadata.create_all(bind=engine)
+        pass
+
+
+    def query(self,query):
+        ResultProxy = connection.execute(query)
+        ResultSet = ResultProxy.fetchall()
+        return {"data" : [dict(row) for row in ResultSet]}
 
     def all(self):
-        # so basically all this code should be executed first, then additional methods to fetch specific data
         query = db.select([messier])
         ResultProxy = connection.execute(query)
         ResultSet = ResultProxy.fetchall()
         return {"data" : [dict(row) for row in ResultSet]}
+
+
+    def getByMessier(self,id) :
+        query = db.select([messier]).where(messier.c.messier == id)
+        return self.query(query)
+
+    def getByConstellation(self,category) :
+        query = db.select([messier]).where(messier.c.constellation == category)
+        return self.query(query)
+
+    def getByConstellationEN(self,category) :
+        query = db.select([messier]).where(messier.c.constellation_en == category)
+        return self.query(query)
+
+    def getByConstellationFR(self,category) :
+        query = db.select([messier]).where(messier.c.constellation_fr == category)
+        return self.query(query)
+
+    def getByConstellationLATIN(self,category) :
+        query = db.select([messier]).where(messier.c.constellation_latin == category)
+        return self.query(query)
+
+    def getByDiscoverer(self,category) :
+        query = db.select([messier]).where(messier.c.messier == category)
+        return self.query(query)
+
+    def getByNGC(self,category) :
+        query = db.select([messier]).where(messier.c.messier == category)
+        return self.query(query)
+
+    def getByObject_Type(self,category) :
+        query = db.select([messier]).where(messier.c.messier == category)
+        return self.query(query)
+
+    def getBySeason(self,category) :
+        query = db.select([messier]).where(messier.c.messier == category)
+        return self.query(query)
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, jsonify
-from flask_restplus import Resource, Api
-from initialization import Messier, db_session 
+from flask_restplus import Api, Resource
+
+#from initialization import Objects 
+from initialization import engine
+from initialization import Messier, db_session
 
 app = Flask(__name__)
 api = Api(app=app)
-
 messier = Messier()
+
+
+##DEBUT CREATION DES ROUTES 
+
+@api.route("/")
+class Test(Resource):
+    def get(self):
+        res = messier.all()
+        print(res)
+        return render_template('index.html')
+
+
 
 @api.route("/galaxies")
 class Test(Resource):
@@ -14,8 +28,125 @@ class Test(Resource):
         print(res)
         return jsonify(res)
 
+@api.route("/galaxies/messier/<id>")
+class Test(Resource):
+    def get(self,id):
+        res = messier.getByMessier(id)
+        print(res)
+        return jsonify(res)
+
+
+@api.route("/galaxies/constellation/<category>")
+class Test(Resource):
+    def get(self,category):
+        res = messier.getByConstellation(category)
+        print(res)
+        return jsonify(res)
+
+@api.route("/galaxies/constellation_en/<category>")
+class Test(Resource):
+    def get(self,category):
+        res = messier.getByConstellationEn(category)
+        print(res)
+        return jsonify(res)
+
+@api.route("/galaxies/constellation_fr/<category>")
+class Test(Resource):
+    def get(self,category):
+        res = messier.getByConstellationFr(category)
+        print(res)
+        return jsonify(res)
+
+
+@api.route("/galaxies/constellation_latin/<category>")
+class Test(Resource):
+    def get(self,category):
+        res = messier.getByConstellationLatin(category)
+        print(res)
+        return jsonify(res)
+
+@api.route("/galaxies/discoverer/<category>")
+class Test(Resource):
+    def get(self,category):
+        res = messier.getByDiscoverer(category)
+        print(res)
+        return jsonify(res)
+
+
+@api.route("/galaxies/ngc/<category>")
+class Test(Resource):
+    def get(self,category):
+        res = messier.getByNGC(category)
+        print(res)
+        return jsonify(res)
+
+
+@api.route("/galaxies/object_type/<path:category>")
+class Test(Resource):
+    def get(self,category):
+        res = messier.getByObjectType(category)
+        print(res)
+        return jsonify(res)
+
+
+@api.route("/galaxies/season/<path:category>")
+class Test(Resource):
+    #category = request.args.get('username')
+    def get(self,category):
+        res = messier.getBySeason(category)
+        print(res)
+        return jsonify(res)
+
+        
+### FIN DES ROUTES
+
+
+
+
+
+
+
+
+
+##Test def
+
+    def post(self):
+        """
+        Add a new galaxy to the list
+        """
+        data = messier.get_json()
+        if not data:
+            data = {"response": "ERROR"}
+            return data, 404
+        else:
+            name = data.get('name')
+            if name:
+                if engine.galaxies.find_one({"name": name}):
+                    return {"response": "galaxy already exists."}, 403
+                else:
+                    engine.insert(data)
+
+
+
+
+
+@api.route("/galaxies/<string:name>")
+class galaxy(Resource):
+    def put(self, name):
+        """
+        Edits a selected galaxy
+        """
+        data = messier.get_json()
+        mydb.galaxies.update({'Name': name}, {'set': data})
+    def delete(self, name):
+        """
+    Delete a selected galaxy
+    """
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
+
+
 
 app.run(host="0.0.0.0", port=80, debug=True)
